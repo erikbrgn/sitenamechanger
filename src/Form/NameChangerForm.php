ea6cef3db1278dcda952fb3ff84de4fa4a875662
Erik Berggren <94erbe28@gmail.com>
2019-04-18 22:30:19 +0200

    Added function to retrieve current name of site

--- src/Form/NameChangerForm.php
+++ src/Form/NameChangerForm.php
@@ -8,23 +8,22 @@ use Drupal\Core\Form\FormStateInterface;
 /**
  * Implements an example form.
  */
-class ExampleForm extends FormBase {
+class NameChangerForm extends FormBase {
 
   /**
    * {@inheritdoc}
    */
   public function getFormId() {
-    return 'example_form';
+    return 'sitename_changer';
   }
 
   /**
    * {@inheritdoc}
    */
   public function buildForm(array $form, FormStateInterface $form_state) {
-    $site_name = \Drupal::config('system.site')->get('name');
     $form['site_name'] = [
       '#type' => 'textfield',
-      '#title' => $this->t($site_name),
+      '#title' => $this->t("Current site name: " . $this->getSiteName()),
     ];
     $form['actions']['#type'] = 'actions';
     $form['actions']['submit'] = [
@@ -39,7 +38,10 @@ class ExampleForm extends FormBase {
    * {@inheritdoc}
    */
   public function validateForm(array &$form, FormStateInterface $form_state) {
-    /* No need to validate input since site name can be anything */
+    /* Checks to see if specified name is the same as the current site name */
+    if ($form_state->getValue('site_name')==$this->getSiteName()) {
+      $form_state->setErrorByName('site_name', $this->t('Site name is already set to '. $form_state->getValue('site_name')));
+    }
   }
 
   /**
@@ -50,7 +52,12 @@ class ExampleForm extends FormBase {
     $config->set('name', $form_state->getValue('site_name')); 
     $config->save();
     // Displaying success message. Fetching saved site name instead of textfield value.
-    drupal_set_message($this->t('Site name has been changed to "@name"', ['@name' => $config->get('name')]));
+    drupal_set_message($this->t('Site name has been changed to "@name"', ['@name' => $this->getSiteName()]));
   }
 
+  // method to retrieve current name of site
+  private function getSiteName () {
+    $site_name = \Drupal::config('system.site')->get('name');
+    return $site_name;
+  }
 }
